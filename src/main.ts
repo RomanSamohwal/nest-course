70a960737853e761e50f8fb5cb974224c10d7d4a
Roman <rsamohwal@gmail.com>
2022-04-22 00:51:58 +0300

    commit

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from "@nestjs/core";
+import {AppModule} from "./app.module";
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+async function start() {
+    const PORT = process.env.PORT || 5000;
+    const app = await NestFactory.create(AppModule)
+
+    const config = new DocumentBuilder()
+        .setTitle('Nest Backend')
+        .setDescription('Документация Rest API')
+        .setVersion('1.0.0')
+        .addTag('Roman')
+        .build()
+
+    const document = SwaggerModule.createDocument(app, config)
+    SwaggerModule.setup('/api/docs', app, document)
+
+    await app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
 }
-bootstrap();
+
+start()
