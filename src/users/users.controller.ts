c827849e408f95df207f0936d204264cdb32ef12
Roman <rsamohwal@gmail.com>
2022-04-24 14:29:54 +0300

    add roles.guard.ts

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,27 +1,30 @@
 import { Body, Controller, Get, Post, UseGuards } from "@nestjs/common";
 import { CreateUserDto } from "./dto/create-user.dto";
 import { UsersService } from "./users.service";
-import {ApiOperation, ApiResponse, ApiTags} from "@nestjs/swagger";
+import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
 import { User } from "./users.model";
-import {JwtAuthGuard} from "../auth/jwt-auth.guard";
+import { Roles } from "../auth/roles-auth.decorator";
+import { RolesGuard } from "src/auth/roles.guard";
 
-@ApiTags('Пользователи')
+@ApiTags("Пользователи")
 @Controller("users")
 export class UsersController {
 
   constructor(private usersService: UsersService) {
   }
 
-  @ApiOperation({summary: 'Создание пользователя'})
-  @ApiResponse({status: 200, type: User})
+  @ApiOperation({ summary: "Создание пользователя" })
+  @ApiResponse({ status: 200, type: User })
   @Post()
   create(@Body() userDto: CreateUserDto) {
     return this.usersService.createUser(userDto);
   }
 
-  @ApiOperation({summary: 'Получение всех пользователей'})
-  @ApiResponse({status: 200, type: [User]})
-  @UseGuards(JwtAuthGuard)
+  @ApiOperation({ summary: "Получение всех пользователей" })
+  @ApiResponse({ status: 200, type: [User] })
+  @Roles("ADMIN")
+  @UseGuards(RolesGuard)
+  // @UseGuards(JwtAuthGuard)
   @Get()
   getAll() {
     return this.usersService.getAllUsers();
